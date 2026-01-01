5ad95f4b35cf209b2be580c3e0d9cc59b192b406
absit23 <113936268+absit23@users.noreply.github.com>
2026-01-01 22:38:55 +0300

    Update block.py
    Signed-off-by: absit23 <113936268+absit23@users.noreply.github.com>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad
+from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, BasicConv, ChannelPool, SpatialGate, TripletAttention, autopad
 from .transformer import TransformerBlock
 
 __all__ = (
